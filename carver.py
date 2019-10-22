fe0f544a8e388f3ee33f845c82a993d9f48ddb30
Jay Hayward <justin.hayward@colorado.edu>
2019-10-21 18:04:48 -0600

    jpg carver
    function that carves jpg files and puts them in a folder

--- carver.py
+++ carver.py
@@ -3,6 +3,7 @@
 # Project 2
 
 from hashlib import md5
+from _md5 import md5
 import sys
 import binascii
 import os
@@ -18,14 +19,25 @@ def main():
     with open(infile, 'rb') as f_obj:  # open stream in binary to read, put in object
         data = f_obj.read()
 
-    # os.mkdir("hayward")  # make directory to put finished carved data
+    os.mkdir("hayward")  # make directory to put finished carved data
 
-    #test data dump
-    print(data[:50])
+    find_carves_jpg(data)
 
-    #test offset find
-    print(hex(data.find(jpg_mark[0])))
-    print(hex(data.find(jpg_mark[1])))
+
+def find_carves_jpg(data):  # carves = files I'm extracting
+    # will likely find a lot of false positives due to frequency of magic number pattern
+    name_c = 1  # name given to new carved file
+    sof = data.find(jpg_mark[0])  #start of file
+    while sof != -1:
+        eof = data.find(jpg_mark[1], sof + 1)  # must find the eof at a position AFTER the sof
+        while eof != -1:  # check the current sof with every eof
+            carve = data[sof:eof]  # range of bytes in which the file is found
+            with open("./hayward/" + str(name_c) + ".jpg", 'wb') as name_object:  #write files to folder
+                print('found file')
+                name_object.write(carve)
+
+            eof = data.find(jpg_mark[1], eof + 1)  # update to next eof
+        sof = data.find(jpg_mark[0], sof + 1)  # update to next sof
 
 
 if __name__ == "__main__":
